246ef32f2cd15a74c4c693b173e09c5303032cb9
VictorAndresDlh <vadela@unal.edu.co>
2021-06-24 12:07:17 -0500

    Update ProyectoRAF.R

--- ProyectoRAF.R
+++ ProyectoRAF.R
@@ -16,123 +16,64 @@ library(tidyverse)
 library(ggplot2)
 library(arsenal)
 library(ordinal)
+library(lubridate)
 
 # Se cargan las bases de datos
 
 # Base de datos para el grupo 8
-G8 <- read_delim("C:/Users/victo/OneDrive/Escritorio/Proyecto Riesgo Actuarial y Financiero/Grupo_P8.txt", 
+G8 <- as.data.frame(read_delim("C:/Users/victo/OneDrive/Escritorio/Proyecto Riesgo Actuarial y Financiero/Grupo_P8.txt", 
                  "|", escape_double = FALSE, col_types = cols(FECINICIO = col_datetime(format = "%Y-%m-%d"), 
                                                               FECFIN = col_datetime(format = "%Y-%m-%d"), 
                                                               PTD = col_logical(), PPD = col_logical(), 
                                                               PH = col_logical(), PPH = col_logical(), 
-                                                              RC = col_logical()), trim_ws = TRUE)
+                                                              RC = col_logical()), trim_ws = TRUE))
 # Base de datos de los Siniestros Históricos
-SINH <- read_delim("C:/Users/victo/OneDrive/Escritorio/Proyecto Riesgo Actuarial y Financiero/Siniestros_Hist.txt", 
+SINH <- as.data.frame(read_delim("C:/Users/victo/OneDrive/Escritorio/Proyecto Riesgo Actuarial y Financiero/Siniestros_Hist.txt", 
                    "|", escape_double = FALSE, col_types = cols(FECHASIN = col_date(format = "%Y-%m-%d"), 
                                                                 FECPAGOAMP = col_date(format = "%Y%m%d")), 
-                              trim_ws = TRUE)
+                              trim_ws = TRUE))
 
-str(G8)
+# Resumen de las dos bases de datos
 summary(G8)
+summary(SINH)
 
-# ANALISIS DE G8
-# Histograma y distribucion de "Valor de la prima suscrita incluida RC"
-G8 %>%
-  select(VLRPRISUSCR) %>%
-  ggplot(aes(x = VLRPRISUSCR)) +
-  geom_histogram(colour = "white")
+# Depuracion de los datos
+# Se generan nuevos dataframe G8T y SINHT
+G8T <- G8
+SINHT <- SINH
 
-descdist(as.numeric(unlist(G8[,3])), boot = 300, discrete  = FALSE)
-plot(fitdist(as.numeric(unlist(G8[,3])), "norm"))
+# G8T
+# Eliminar los registros que tenian prima suscrita = 0
+G8T <- subset(G8, G8[3] > 0)
+# Cambiar RC = T si efectivamente VLRPRSRC > 0
+for (row in 1:nrow(G8T)) {
+  if (G8T$VLRPRISUSCR > 0) {
+    G8T$RC = T
+  }
+}
+rm(row)
 
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-G8 %>%
-  select(VLRASEGU) %>%
-  ggplot(aes(x = VLRASEGU)) +
-  geom_histogram(colour = "white")
+# SINHT
+# Escoger unicamente los valores que tenian como estado final "pagado" 
+SINHT <- subset(SINHT, SINHT$ESTADO_FINAL == "Pagado")
+# Escoger unicamente los valores asegurados mayores que 0
+SINHT <- subset(SINHT, SINHT$VLRASEGU > 0)
+# Escoger unicamente los datos correspondientes al 2018
+SINHT <- subset(SINHT, year(SINHT$FECHASIN) > 2017)
 
-descdist(as.numeric(unlist(G8[,4])), boot = 300, discrete  = FALSE)
-plot(fitdist(as.numeric(unlist(G8[,4]))/10^9, "beta"))
+# Encontrar la mejor distribucion para el posible monto de reclamacion
+descdist(as.numeric(as.vector(unlist(SINHT$VLRSININCUR))), boot = 300,  discrete = F)
+x <- fitdist(as.numeric(as.vector(unlist(SINHT$VLRSININCUR))), "gamma", method = "mme")
+plot(x)
 
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-G8 %>%
-  select(VLRASEGURC) %>%
-  ggplot(aes(x = VLRASEGURC)) + 
-  geom_histogram(colour = "white")
+# Proceso de conteo
+dias <- count(SINHT, SINHT$FECHASIN)
+semana <- count(SINHT, week(SINHT$FECHASIN))
+meses <- count(SINHT, month(SINHT$FECHASIN))
 
-descdist(as.numeric(unlist(G8[,5])), boot = 300, discrete  = FALSE)
-plot(fitdist(as.numeric(unlist(G8[,5])), "unif"))
-
-#ANALISIS DE SINH
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRPRIMAPAG) %>% 
-  ggplot(aes(x = VLRPRIMAPAG)) +
-  geom_density()
-
-descdist(as.numeric(unlist(SINH[,2])), boot = 300, discrete  = FALSE)
-plot(fitdist(as.numeric(unlist(SINH[,2])), "norm"))
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRPRISUSCR) %>% 
-  ggplot(aes(x = VLRPRISUSCR)) +
-  geom_histogram(colour = "white")
-
-descdist(as.numeric(unlist(SINH[,3])), boot = 300,  discrete  = FALSE)
-plot(fitdist(as.numeric(unlist(SINH[,2])), "exp"))
-
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRASEGU) %>% 
-  ggplot(aes(x = VLRASEGU)) +
-  geom_histogram(colour = "white")
-
-descdist(as.numeric(unlist(SINH[,4])), discrete  = FALSE)
-
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRPAGADO.) %>% 
-  ggplot(aes(x = VLRPAGADO.)) +
-  geom_histogram(colour = "white")
-
-descdist(as.numeric(unlist(SINH[,6])), discrete  = FALSE)
-
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRSININCUR) %>% 
-  ggplot(aes(x = VLRSININCUR)) +
-  geom_histogram(colour = "white")
-
-descdist(as.numeric(unlist(SINH[,7])), discrete  = FALSE)
-
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRDEDUCIBLE) %>% 
-  ggplot(aes(x = VLRDEDUCIBLE)) +
-  geom_histogram(colour = "white")
-
-descdist(as.numeric(unlist(SINH[,8])), discrete  = FALSE)
-
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRRECOBRO) %>% 
-  ggplot(aes(x = VLRRECOBRO)) +
-  geom_histogram(colour = "white")
-
-descdist(as.numeric(unlist(SINH[,9])), discrete  = FALSE)
-
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRRSVACONSTIAMP) %>% 
-  ggplot(aes(x = VLRRSVACONSTIAMP)) +
-  geom_histogram(colour = "white")
-
-descdist(as.numeric(unlist(SINH[,10])), discrete  = FALSE)
-
-# Histograma y distribucion de Valor de la prima suscrita incluida RC
-SINH %>% 
-  select(VLRRSVAPAGAMP) %>% 
-  ggplot(aes(x = VLRRSVAPAGAMP)) +
-  geom_histogram(colour = "white")
-
-descdist(as.numeric(unlist(SINH[,11])), boot = 300, discrete  = F)
+descdist(dias$n,boot = 50, discrete = T)
+plot(fitdist(dias$n, "pois"))
+plot(fitdist(dias$n, "nbinom"))
+descdist(semana$n,boot = 50, discrete = T)
+plot(fitdist(dias$n, "pois"))
+plot(fitdist(semana$n, "nbinom", method = "mge"))
